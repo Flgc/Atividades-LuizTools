e8b12c10a9deb2ed2a624d7d7a6db779ea007407
Fabio Luis <ml@fabiotecnico1.biz>
2021-02-21 16:46:12 -0300

    Autenticação em Node.js com Passport - Ajustes em login.js e App.js

--- nodejs-passport-login/routes/login.js
+++ nodejs-passport-login/routes/login.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router =  express.Router();
 
-
-/* GET login page */
 router.get('/', (req, res, next) => {
-   res.render('Login', { title: 'Login', message: null});
+   res.render('login', {message: null});
 });
 
-modeule.exports = router;
+module.exports = router;
